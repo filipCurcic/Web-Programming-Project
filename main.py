e2d21e51cff25ee42c440375ddd4ef5baabef8c9
Filip Curcic <filipcurcic42@gmail.com>
2018-01-14 22:15:47 +0100

    Update certain files

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ import flask
 from flask import Flask
 from flaskext.mysql import MySQL
 from flask import request
-
+from flask import session
+from flask import redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__, static_url_path="")
 
 mysql = MySQL(cursorclass=pymysql.cursors.DictCursor)
+
+app.secret_key = "SECRET_KEY"
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
 
 app.config["MYSQL_DATABASE_USER"] = "root"
 app.config["MYSQL_DATABASE_PASSWORD"] = "filipcurcic1997"
@@ -16,6 +27,8 @@ app.config["MYSQL_DATABASE_DB"] = "movies_db"
 app.config["MYSQL_DATABASE_HOST"] = "localhost"
 
 mysql.init_app(app)
+
+
 
 @app.route("/")
 @app.route("/index")
@@ -48,6 +61,23 @@ def popular():
 
     return flask.jsonify(rows)
 
+@app.route("/actors", methods=["GET"])
+def getActors():
+
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM movie_person INNER JOIN person ON movie_person.person_idperson = person.idperson")
+    rows = cursor.fetchall()
+
+    return flask.jsonify(rows)
+
+@app.route("/directors", methods=["GET"])
+def getDirectors():
+
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM movie_person INNER JOIN person ON movie_person.person_idperson = person.idperson WHERE role_idrole=2")
+    rows = cursor.fetchall()
+
+    return flask.jsonify(rows)
 
 
 @app.route('/register', methods=["POST"])
@@ -69,6 +99,35 @@ def registration():
     db.commit()
 
     return flask.jsonify({"status": "done"}), 201
+
+@app.route('/login', methods=["POST"])
+def login():
+    login_user = request.json
+    db = mysql.get_db()
+    cursor = mysql.get_db().cursor()
+    cursor.execute("SELECT * FROM user WHERE username=%s AND password=%s", (login_user["username"], login_user["password"]))
+    user = cursor.fetchone()
+    if user is not None:
+        session["user"] = user
+
+
     
+    return flask.jsonify(session["user"])
+
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['/images'], filename))
     
 app.run("0.0.0.0", 80, threaded=True)
--- static/app/filmovi/filmovi.js
+++ static/app/filmovi/filmovi.js
@@ -7,6 +7,13 @@
         lv.filmovi = [];
         lv.ratings = [];
         lv.pop = [];
+        lv.loggedin = {};
+        lv.username = "";
+        lv.password = "";
+        lv.people = [];
+        lv.selecteedActor =[];
+        lv.directors = [];
+        lv.selectedDirector = [];
 
         lv.dobaviFilmove = function() {
             $http.get("/filmovi").then(function(response) {      
@@ -29,17 +36,43 @@
                 console.log(reason);
             });
         }
+        
+        lv.checkLogin = function () {
+            $http.post("/login", {"username": lv.username, "password": lv.password}).then(function (response) {
+                lv.loggedin = response.data;
+                console.log(lv.loggedin);
+                alert("You have succefully logged in");
+                
+            }, function (reason) {
+                alert("You have entered an incorrect username or password!")
+            });
+        }
 
         lv.search = function() {
             var input, filter;
             input = document.getElementById("movSearch");
             
         }
+        lv.dobaviLjude = function() {
+            $http.get("/actors").then(function(response) {      
+                lv.people = response.data;
+            }, function(reason) {
+                console.log(reason);
+            });
+        }
+        lv.getDirectors = function() {
+            $http.get("/directors").then(function(response) {      
+                lv.directors = response.data;
+            }, function(reason) {
+                console.log(reason);
+            });
+        }
 
 
         lv.dobaviRatings();
         lv.dobaviFilmove();
         lv.najPop();
-
+        lv.dobaviLjude();
+        lv.getDirectors();
     });
 })(angular);
